bc463a4e4bdff2b610384a83dcca3633672b571f
Evanson <evansonmwangi83@gmail.com>
2016-02-10 07:57:23 +0300

    An Online Movie Tickets Booking App

---
+++ index.js
@@ -0,0 +1,24 @@
+/**
+ * Created by mars on 2/10/16.
+ */
+/**This will be our Server**/
+var express = require("express"),
+    logger = require("express-logger"),
+    json = require("express-json"),
+    bodyParser = require("body-parser"),
+    fs = require('fs'),
+    port = process.env.PORT || 2595;
+
+var app = express();
+app.use(logger({path: "public/log/log.txt"}));
+app.use(json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("view options", {
+    layout: false
+});
+app.use(express.static(__dirname + '/public'));
+app.get('/', function (req, res) {
+    res.render('public/index.html');
+});
+app.listen(port);
+console.log('Express server running at http://localhost: ' + port);
